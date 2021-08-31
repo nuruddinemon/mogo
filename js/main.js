99310debb7a8775187ac6851da4fa06759c06b2f
N-U-Emon <76093791+N-U-Emon@users.noreply.github.com>
2021-08-31 20:54:09 +0600

    add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// counter up
+$(document).ready(function() {
+    $('.counter').counterUp({
+        delay: 20,
+        time: 3000
+    });
+    
+});
+
+// picturehover
+
+$(document).ready(function(){
+    $('.about_img').picturehover();
+});
+
